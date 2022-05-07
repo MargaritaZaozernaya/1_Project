3d3317b151d92df686e9cd4921ffa61e2b9cb5b1
MargaritaZaozernaya <m.zaozerna@gmail.com>
2022-05-07 19:35:15 +0300

    New line was added

---
+++ 001_simpleTest.py
@@ -0,0 +1,25 @@
+# years
+current_year = 2022
+year_of_birth = 1988
+user_age = current_year - year_of_birth
+
+# code parts
+code_1 = '354'
+code_3 = 132
+
+# name
+user_name = 'John'
+user_surname = 'Smith'
+
+# code 2 data
+x = 152
+y = 132
+a = x / y
+b = a * 13
+c = 2
+d = b**(1./c)
+e = int(d)
+code_2 = e
+
+print('Helo ' + user_name + ' ' + user_surname + '. Your are '+str(user_age)+' years old. Your secret code is '+str(code_1)+'-'+str(code_2)+'-'+str(code_3)+'.')
+
